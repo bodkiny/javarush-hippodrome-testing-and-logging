a3ae4693c8140c9f2c8de27a60fc500da8c3b8f1
Bohdan Rudenko <brudenko2804@gmail.com>
2024-02-19 12:22:09 +0200

    Add and configure logging for the project

--- src/main/java/Horse.java
+++ src/main/java/Horse.java
@@ -1,5 +1,9 @@
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
+
 import static java.util.Objects.isNull;
 
+@Log4j2
 public class Horse {
 
     private final String name;
@@ -8,20 +12,25 @@ public class Horse {
 
     public Horse(String name, double speed, double distance) {
         if (isNull(name)) {
+            LOGGER.error("Name is null");
             throw new IllegalArgumentException("Name cannot be null.");
         } else if (name.isBlank()) {
+            LOGGER.error("Name is blank");
             throw new IllegalArgumentException("Name cannot be blank.");
         }
         if (speed < 0) {
+            LOGGER.error("Speed is negative");
             throw new IllegalArgumentException("Speed cannot be negative.");
         }
         if (distance < 0) {
+            LOGGER.error("Distance is negative");
             throw new IllegalArgumentException("Distance cannot be negative.");
         }
 
         this.name = name;
         this.speed = speed;
         this.distance = distance;
+        LOGGER.debug("Horse creation, name [{}], speed [{}]", name, speed);
     }
 
     public Horse(String name, double speed) {
